4606f31969fa045f5ac383935110ea7141fe077f
Sunil Kr. Singh <sunil.singh@economical.com>
2022-06-04 19:54:19 -0400

    cleaning up trial checkin

--- starter/app/src/main/java/com/udacity/asteroidradar/domain/PictureOfDayDM.kt
+++
@@ -1,9 +0,0 @@
-package com.udacity.asteroidradar.domain
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class PictureOfDayDM(val mediaType:String,
-                     val title: String,
-                     val url:String ) : Parcelable
